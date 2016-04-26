5b98dc31e65eca3fbb7b2a8d303d8c57388e9721
Ovidijus Stukas <ovidijus.stukas@ktu.edu>
2016-04-26 10:05:43 +0300

    added logout feature

--- src/wms-website/src/main/webapp/resources/js/custom/home.js
+++ src/wms-website/src/main/webapp/resources/js/custom/home.js
@@ -1,19 +1,21 @@
-/**
-    * Created by CosISolutions on 2016-02-12.
- */
-
 $(document).ready(function () {
-    'use strict';
-    var $win = $(window);
-    var $navbar = $('#main-navbar');
+  'use strict';
+  var $win = $(window);
+  var $navbar = $('#main-navbar');
 
-    /**
-     * Binds scroll of page to navbar
-     * Changes opacity levels upon scrolling
-     * Takes a look for navbar height since there is nothing behind it at top and hole it's height
-     */
-    $win.scroll(function () {
-        $win.scrollTop() - $navbar.height() <= 0 ? $navbar.css('opacity', '1') : $navbar.css('opacity', '0.8');
-    });
+  /**
+   * Binds scroll of page to navbar
+   * Changes opacity levels upon scrolling
+   * Takes a look for navbar height since there is nothing behind it at top and hole it's height
+   */
+  $win.scroll(function () {
+    $win.scrollTop() - $navbar.height() <= 0 ? $navbar.css('opacity', '1') : $navbar.css('opacity', '0.8');
+  });
 });
+
+function logout() {
+  var form = document.getElementById("logoutForm") || '';
+  if(form !== '')
+    form.submit();
+}
 
