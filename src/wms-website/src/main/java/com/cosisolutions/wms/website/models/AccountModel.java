d4e99beaa8742c118d337d3afd348161da161b7f
Ovidijus Stukas <ovidijus.stukas@ktu.edu>
2016-05-20 21:28:30 +0300

    Adding account schema, entity, model

---
+++ src/wms-website/src/main/java/com/cosisolutions/wms/website/models/AccountModel.java
@@ -0,0 +1,60 @@
+package com.cosisolutions.wms.website.models;
+
+public class AccountModel {
+
+  private int id;
+  private String email;
+  private String password;
+  private String passwordRepeat;
+  private String lastName;
+  private String firstName;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id)
+  {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPasswordRepeat() {
+    return passwordRepeat;
+  }
+
+  public void setPasswordRepeat(String passwordRepeat) {
+    this.passwordRepeat = passwordRepeat;
+  }
+}
