44ca53bff592521b4f2fd6e7c7ca38bceaf4f5a1
Ovidijus Stukas <ovidijus.stukas@ktu.edu>
2016-05-21 12:37:44 +0300

    Added item group mapper

---
+++ src/wms-website/src/main/java/com/cosisolutions/wms/website/mapper/ItemGroupMapper.java
@@ -0,0 +1,30 @@
+package com.cosisolutions.wms.website.mapper;
+
+import com.cosisolutions.wms.website.entity.ItemGroupEntity;
+import com.cosisolutions.wms.website.models.ItemGroupModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemGroupMapper implements IMapper<ItemGroupEntity, ItemGroupModel> {
+  @Autowired
+  private AssetMapper assetMapper;
+
+  @Override
+  public void toModel(ItemGroupModel itemGroupModel, ItemGroupEntity itemGroupEntity) {
+    itemGroupModel.setId(itemGroupEntity.getId());
+    itemGroupModel.setName(itemGroupEntity.getName());
+
+    assetMapper.toModel(itemGroupModel.getAsset(), itemGroupEntity.getAsset());
+  }
+
+  @Override
+  public void toEntity(ItemGroupEntity itemGroupEntity, ItemGroupModel itemGroupModel) {
+    itemGroupEntity.setName(itemGroupModel.getName());
+
+    if(itemGroupModel.getId() > 0)
+      itemGroupEntity.setId(itemGroupModel.getId());
+
+    assetMapper.toEntity(itemGroupEntity.getAsset(), itemGroupModel.getAsset());
+  }
+}
