37d8420044efb0ffc9793d1c13db84342b25f233
Ovidijus Stukas <ovidijus.stukas@ktu.edu>
2016-05-21 13:51:52 +0300

    removed redundant asset code

--- src/wms-website/src/main/java/com/cosisolutions/wms/website/controller/AssetsController.java
+++ src/wms-website/src/main/java/com/cosisolutions/wms/website/controller/AssetsController.java
@@ -1,26 +1,17 @@
 package com.cosisolutions.wms.website.controller;
 
-import com.cosisolutions.wms.website.models.AssetModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Created by CosISolutions on 2016-04-26.
- */
-
 @Controller
 @RequestMapping(value = {"/assets"})
 public class AssetsController {
 
-    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
-    public ModelAndView index() {
-        return new ModelAndView("assets/management");
-    }
-
     @RequestMapping(value = {"add"}, method = RequestMethod.GET)
     public ModelAndView add() {
+
         return new ModelAndView("assets/add");
     }
 }
